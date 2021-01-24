af10a08593da7300154e1f3d289b486d21494ecf
Tunay Özdemir <tunayozdemir@Tunay-MacBook-Pro.local>
2021-01-24 03:04:28 +0300

    key kullanımı

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import './App.css';
+import React, { Component } from 'react'
 import Giris from './pages/giris'
 import User from './pages/user'
+import Users from './component/users'
 import Navbar from './component/navbar'
+import './App.css';
 
 
 
-const App = () => {
+ class App extends Component {
 
-  return (
-    <div className="container">
-      <Giris />
-      <User 
-        name ="Hayriye Özdemir"
-        department = "Yazılım"
-        salary="5000"
-      />
-      <User 
-        name ="Tunay Özdemir"
-        department = "Yazılım"
-        salary="3000"
-      />
-      <hr/>
-      <Navbar title="User App"/>
+  state = {
+    users :[
+      {
+        id:1,
+        name :"Tunay Özdemir",
+        salary :"5000",
+        department:"Bilişim"
+      },
+      {
+        id:2,
+        name :"Hayriye Özdemir",
+        salary :"6000",
+        department:"Yönetim"
+      },
+      {
+        id:3,
+        name :"Asya Özdemir",
+        salary :"1000",
+        department:"Tekniker"
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Giris />
+        <Users users={this.state.users}/>
+        <hr/>
+        <Navbar title="User App"/>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
+
--- src/pages/user.js
+++ src/pages/user.js
@@ -6,9 +6,7 @@ class User extends Component {
 
   constructor(props){
     super(props);
-
     this.state={
-      // deneme : "Deneme"
       isVisable : false
     }
   }
@@ -19,6 +17,15 @@ class User extends Component {
       department : "Bilgi yok",
       salary : "Bilgi yok"
   }
+
+  // isVisable false ise true, true ise false olacak
+  onClickEvent = (e) => {
+    this.setState({
+      isVisable : !this.state.isVisable
+    })
+
+  }
+
 
   render() {
     
@@ -43,7 +50,7 @@ class User extends Component {
     <div className="col-md-8 mb-4">
       <div className="card">
         <div className="card-header d-flex justify-content-between">
-        <h4 className="d-inline">{name}</h4>
+        <h4 className="d-inline" style={{cursor:"pointer"}}  onClick={this.onClickEvent.bind(this,34)}>{name}</h4>
         <i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
         </div>
 
